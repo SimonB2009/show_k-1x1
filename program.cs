4dc39400ce956075fd03a79a23dd996bffa8ad03
SimonB2009 <brandstetter.simon@web.de>
2022-12-07 19:35:31 +0100

    add already existing files

---
+++ program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace simon {
+
+    class Show_k_1x1 {
+
+        static void Main(string[] args) {
+
+            for (int a =1; a<11; a++) {
+
+                Console.WriteLine("    ");
+
+                for (int b = 1; b<11; b++) {
+
+                    Console.WriteLine(a + " * " + b + " = " + (a*b));
+                }
+            }
+        }
+    }
+}
